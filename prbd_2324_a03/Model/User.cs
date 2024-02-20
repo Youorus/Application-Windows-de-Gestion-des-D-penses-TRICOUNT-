d03075d2c52f6e9464759820af8b14e285b83560
Benoît Penelle <benoit@penelle.net>
2024-02-20 23:05:03 +0100

    feat: version initiale

---
+++ prbd_2324_a03/Model/User.cs
@@ -0,0 +1,8 @@
+﻿using PRBD_Framework;
+
+namespace prbd_2324_a03.Model;
+
+public class User : EntityBase<PridContext>
+{
+    public int UserId { get; set; }
+}
---
+++ prbd_2324_a03/ViewModel/MainViewModel.cs
@@ -0,0 +1,8 @@
+﻿using prbd_2324_a03.Model;
+
+namespace prbd_2324_a03.ViewModel;
+
+public class MainViewModel : PRBD_Framework.ViewModelBase<User, PridContext>
+{
+    public string Title => "prbd_2324_a03";
+}
