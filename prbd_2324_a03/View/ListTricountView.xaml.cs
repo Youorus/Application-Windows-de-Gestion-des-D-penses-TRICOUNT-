bc1a70504fa994dada36a1a629e90547fa108b80
Marc Junior NKUE TAKOUMBA <2911mankuetakoumba@student.epfc.eu>
2024-05-04 12:35:01 +0200

    (Tricount_Card): implementattion de la vue d'une card d'un tricount

---
+++ prbd_2324_a03/View/ListTricountView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PRBD_Framework;
+
+
+namespace prbd_2324_a03.View
+{
+  
+    public partial class ListTricountView : UserControl
+    {
+        public ListTricountView() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ prbd_2324_a03/ViewModel/ListTricountViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PRBD_Framework;
+using System.Threading.Tasks;
+
+namespace prbd_2324_a03.ViewModel
+{
+    public class ListTricountViewModel : ViewModelCommon
+    {
+    }
+}
